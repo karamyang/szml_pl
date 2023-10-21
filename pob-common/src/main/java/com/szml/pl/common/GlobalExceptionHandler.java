e59433c459e8f5ad36be7ba2e61ebc0f317a3fb8
杨云翔 <9913111+yang-yunxiang111@user.noreply.gitee.com>
2023-10-21 21:43:14 +0800

    服务模块搭建

---
+++ pob-common/src/main/java/com/szml/pl/common/GlobalExceptionHandler.java
@@ -0,0 +1,34 @@
+package com.szml.pl.common;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @description: 全局异常捕获
+ * @author：karma
+ * @date: 2023/10/21
+ */
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    /**
+     * 处理自定义异常
+     *
+     */
+    @ExceptionHandler(value = DefinitionException.class)
+    @ResponseBody
+    public Result bizExceptionHandler(DefinitionException e) {
+        return Result.buildResult(e.errorCode.toString(),e.errorMsg);
+    }
+
+    /**
+     * 处理其他异常
+     *
+     */
+    @ExceptionHandler(value = Exception.class)
+    @ResponseBody
+    public Result exceptionHandler( Exception e) {
+        return Result.buildErrorResult();
+    }
+}
