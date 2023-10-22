c84b91cd77ad0dde129c7dbfc9982e4bff07656e
demowu <demowu@demo.cn>
2023-10-22 10:12:45 +0800

    完善项目结构，编写启动类

---
+++ pob-product/product-bootstrap/src/test/java/com/szml/pl/ApplicationTest.java
@@ -0,0 +1,17 @@
+package com.szml.pl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @description:
+ * @author：wufengning
+ * @date: 2023/10/22
+ */
+@SpringBootTest
+public class ApplicationTest {
+    @Test
+    void test(){
+        System.out.println("启动成功");
+    }
+}
