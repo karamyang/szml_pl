75110e37bf58a710c1d39abf1933cc742413bed1
杨云翔 <9913111+yang-yunxiang111@user.noreply.gitee.com>
2023-10-22 10:18:37 +0800

    实体创建

---
+++ pob-admin/admin-model/src/main/java/com/szml/pl/admin/model/Admin.java
@@ -0,0 +1,77 @@
+package com.szml.pl.admin.model;
+
+
+public class Admin {
+
+  private long id;
+  private String username;
+  private String password;
+  private java.sql.Timestamp createTime;
+  private java.sql.Timestamp updateTime;
+  private String telephone;
+  private String email;
+
+
+  public long getId() {
+    return id;
+  }
+
+  public void setId(long id) {
+    this.id = id;
+  }
+
+
+  public String getUsername() {
+    return username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+
+  public java.sql.Timestamp getCreateTime() {
+    return createTime;
+  }
+
+  public void setCreateTime(java.sql.Timestamp createTime) {
+    this.createTime = createTime;
+  }
+
+
+  public java.sql.Timestamp getUpdateTime() {
+    return updateTime;
+  }
+
+  public void setUpdateTime(java.sql.Timestamp updateTime) {
+    this.updateTime = updateTime;
+  }
+
+
+  public String getTelephone() {
+    return telephone;
+  }
+
+  public void setTelephone(String telephone) {
+    this.telephone = telephone;
+  }
+
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+}
