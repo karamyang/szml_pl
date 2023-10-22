02018a6de5833845b1b758b95b72878dc174b8ce
杨云翔 <9913111+yang-yunxiang111@user.noreply.gitee.com>
2023-10-22 12:26:53 +0800

    商品服务Controller的编写

--- pob-product/product-web/src/main/java/com/szml/pl/controller/PobProductSearchController.java
+++ pob-product/product-web/src/main/java/com/szml/pl/controller/PobProductSearchController.java
@@ -1,7 +1,11 @@
 package com.szml.pl.controller;
 
+import com.szml.pl.common.Constants;
+import com.szml.pl.common.Result;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * @description:
@@ -11,4 +15,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/search")
 public class PobProductSearchController {
+    //查询所有商品
+    @RequestMapping(value = "/roductall", method = RequestMethod.POST)
+    @ResponseBody
+    public Result productall(){
+        return Result.buildResult(Constants.ResponseCode.SUCCESS);
+    }
+
+    //条件查询商品
+    @RequestMapping(value = "/conditionproductall", method = RequestMethod.POST)
+    @ResponseBody
+    public Result conditionproductall(){
+        return Result.buildResult(Constants.ResponseCode.SUCCESS);
+    }
+
+    //商品操作记录
+    @RequestMapping(value = "/record", method = RequestMethod.POST)
+    @ResponseBody
+    public Result record(){
+        return Result.buildResult(Constants.ResponseCode.SUCCESS);
+    }
 }
