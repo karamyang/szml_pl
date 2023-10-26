9166db06baa7d432408ffb7be7d29c6e35c4f298
杨云翔 <9913111+yang-yunxiang111@user.noreply.gitee.com>
2023-10-27 03:23:06 +0800

    分库分表

--- pob-product/product-bootstrap/src/main/java/com/szml/pl/ProductApplication.java
+++ pob-product/product-bootstrap/src/main/java/com/szml/pl/ProductApplication.java
@@ -2,6 +2,7 @@ package com.szml.pl;
 
 import org.apache.dubbo.config.spring.context.annotation.DubboComponentScan;
 import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -17,6 +18,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 @EnableScheduling
 @SpringBootApplication
+@MapperScan(basePackages = {"com.szml.pl.dao"})
 public class ProductApplication {
     public static void main(String[] args) {
         SpringApplication.run(ProductApplication.class,args);
--- pob-product/product-service/src/main/java/com/szml/pl/impl/ProductServiceImpl.java
+++ pob-product/product-service/src/main/java/com/szml/pl/impl/ProductServiceImpl.java
@@ -79,7 +79,7 @@ public class ProductServiceImpl extends ServiceImpl<ProductDao, Product> impleme
         }
         else {
             //2.判断是否存在于商品表
-            Product product1 = productDao.selectById(productId);
+            Product product1 = productService.getById(productId);
             //存在则更新商品
             if(product1!=null){
                 //更新商品
@@ -98,9 +98,10 @@ public class ProductServiceImpl extends ServiceImpl<ProductDao, Product> impleme
                 //3.否则添加商品到商品表
                 product.setCreateTime(new Timestamp(System.currentTimeMillis()));
                 //用雪花算法设置全局id
-                product.setId(idGenerator.nextId());
-                int flag = productDao.insert(product);
-                if(flag<0){
+//                product.setId(idGenerator.nextId());
+                product.setId(null);
+                boolean save = productService.save(product);
+                if(!save){
                     return false;
                 }
             }
